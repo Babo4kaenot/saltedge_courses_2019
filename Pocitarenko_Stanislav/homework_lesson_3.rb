d7d749893139e1a69d663c41be6a50b376b87d80
Babo4kaenot <Stanislavgerm@gmail.com>
2019-02-14 12:39:01 +0200

    homework

---
+++ Pocitarenko_Stanislav/homework_lesson_3.rb
@@ -0,0 +1,23 @@
+puts -123.odd?
+
+puts 6893.next
+
+puts 0.pred
+
+puts ((12.to_f)/4.5).round()
+
+puts 28.modulo(6)*4.7.round()
+
+puts "brand new bicycle\r\n".chop
+
+puts "Brand new bicycle".slice!(6..8)
+
+puts "Brand new bicycle".chomp' bicycle'
+
+puts "Brand new bicycle".split.last
+
+puts "brand new bicycle".index('w')
+
+puts "I thought I knew  everything  before that night".rpartition('everything')
+
+puts "Final result from Float/Integer part is: #{-123 + 6894 + 3 + 20 - 1}"
